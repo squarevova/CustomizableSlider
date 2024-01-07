b66dadbadf1306d67e66ac5fcdfb5c84dced1375
Vladimir Milichenko <kventvova@gmail.com>
2024-01-07 14:30:44 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CustomizableSlider",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "CustomizableSlider",
+            targets: ["CustomizableSlider"]),
+    ],
+    targets: [
+        .target(
+            name: "CustomizableSlider")
+    ]
+)
---
+++ Sources/CustomizableSlider/CustomizableSlider.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public struct CustomizableSlider<Value: BinaryFloatingPoint>: View {
+    public var body: some View {
+        Text("Test")
+    }
+}
